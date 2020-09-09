c06915142583be56e3f5319a16dd99bfb7cb0438
Ricardo Hincapie <hincapiet.ricardo@gmail.com>
2020-09-09 22:54:28 +0000

    Add task #4 correction

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,6 +6,7 @@ from api.v1.views import app_views
 
 #app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
+
 @app_views.route('/status', strict_slashes=False)
 def status():
     """Manages /status routing"""

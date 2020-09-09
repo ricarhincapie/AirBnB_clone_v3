cb949df5e87d54cc4a0ae6965ada09b1c738bdfc
Ricardo Hincapie <hincapiet.ricardo@gmail.com>
2020-09-09 22:51:09 +0000

    Add task #4 correction

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,11 +2,11 @@
 """
 initialize the views package
 """
-from api.v1.views.index import app_views
-"""
+#from api.v1.views.index import app_views
+
 from flask import Blueprint
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
 from api.v1.views import *
 from api.v1.views.index import *
-
-app_views = Blueprint('first', url_prefix='/api/v1')
-"""
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """Module to do something"""
 
-#from api.v1.views import app_views
 from flask import Flask, jsonify, Blueprint
+from api.v1.views import app_views
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+#app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 @app_views.route('/status', strict_slashes=False)
 def status():

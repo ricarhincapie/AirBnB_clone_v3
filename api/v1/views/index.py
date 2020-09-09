eb65c7a0ebac920ad0bd8e000d0603b5fe185661
Ricardo Hincapie <hincapiet.ricardo@gmail.com>
2020-09-09 23:34:07 +0000

    Add task #5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,11 +3,30 @@
 
 from flask import Flask, jsonify, Blueprint
 from api.v1.views import app_views
+from models import storage
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models.amenity import Amenity
 
-#app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+classes = {"Amenity": Amenity, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     """Manages /status routing"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """Returns the stats for our application"""
+    dic = {}
+    for key, value in classes.items():
+        dic[key] = storage.count(value)
+        #if tmp > 0:
+        #    dic[key] = tmp
+    return jsonify(dic)

a6138025d445aa8ce398980291dff03a98730ec2
Ricardo Hincapie <hincapiet.ricardo@gmail.com>
2020-09-10 21:25:04 +0000

    Add task #8

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -30,7 +30,9 @@ def get_city(city_id):
         abort(404)
     return jsonify(box.to_dict())
 
-@app_views.route('/cities/<city_id>', methods=['DELETE'], strict_slashes=False)
+
+@app_views.route('/cities/<city_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def delete_city(city_id):
     """Deletes the city w the id provided"""
     box = storage.get(City, city_id)
@@ -40,7 +42,9 @@ def delete_city(city_id):
         return jsonify({}), 200
     abort(404)
 
-@app_views.route('/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
+
+@app_views.route('/states/<state_id>/cities', methods=['POST'],
+                 strict_slashes=False)
 def create_city(state_id):
     """Route to handle states/<state_id>/cities POST"""
     box = request.get_json(silent=True)
@@ -53,18 +57,20 @@ def create_city(state_id):
     storage.save()
     return jsonify(new_city.to_dict()), 201
 
-@app_views.route('cities/<city_id>', methods=["PUT"], strict_slashes=False)
+
+@app_views.route('cities/<city_id>', methods=["PUT"],
+                 strict_slashes=False)
 def update_city(city_id):
     """Route to handle states/<state_id>/cities POST"""
     requ = request.get_json(silent=True)
     if requ is None:
         abort(400, 'Not a JSON')
-    box = storage.get(City, city_id)
-    if box is None:
+    current_city = storage.get(City, city_id)
+    if current_city is None:
         abort(404, 'Not a JSON')
-    for key, value in box:
+    for key, value in requ.items():
         if key in ['id', 'state_id', 'created_at', 'updated_at']:
             pass
-        setattr(box, key, value)
-    return jsonify(box.to_dict()), 200
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+        setattr(current_city, key, value)
+    storage.save()
+    return jsonify(current_city.to_dict()), 200

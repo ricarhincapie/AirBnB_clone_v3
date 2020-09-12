254b430fc630a208aac1c1c080594a307a7c1549
Ricardo Hincapie <hincapiet.ricardo@gmail.com>
2020-09-12 17:48:33 +0000

    Add task 13

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,8 +6,11 @@ from flask import url_for, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 import os
+from flask_cors import CORS
+from os import getenv
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins":"0.0.0.0"}})
 host = os.getenv('HBNB_API_HOST', '0.0.0.0')
 port = os.getenv('HBNB_API_PORT', 5000)
 app.url_map.strict_slashes = False
